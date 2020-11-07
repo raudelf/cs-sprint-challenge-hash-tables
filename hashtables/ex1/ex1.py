312c27a03ad7dc4d5463c081855a11c68eb9baa7
raudelf <rawdell@outlook.com>
2020-11-06 20:33:50 -0600

    :white_check_mark: ex1.py
    Completed the first challenge that returns tuples of weights that equal to the limit

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,18 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    weight_dict = {}
 
-    return None
+    for index, weight in enumerate(weights):
+        weight_dict[weight] = index
+
+    for index, weight in enumerate(weights):
+        diff = limit - weight
+
+        if diff in weight_dict:
+            index_2 = weight_dict[diff]
+
+            if index_2 > index:
+                return (index_2, index)
+            else:
+                return (index, index_2)
